bcebef221d23ae4ee1e4f229f92cb743904e6d7a
EuBain <tayrsi@outlook.com>
2023-10-12 04:29:26 +0800

    nest first

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     dasasdas
     {/* <HashRouter> */}
-    <BrowserRouter>
+    <BrowserRouter basename='/master-react'>
       <App />
     </BrowserRouter>
     {/* </HashRouter> */}
