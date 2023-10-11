066797b74f6abaf1b230895528a8c3c19324082c
EuBain <tayrsi@outlook.com>
2023-10-12 04:21:59 +0800

    nest first

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'virtual:uno.css'
-import {  HashRouter } from 'react-router-dom'
+import {  BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     dasasdas
-    <HashRouter>
-    {/* <BrowserRouter> */}
+    {/* <HashRouter> */}
+    <BrowserRouter>
       <App />
-    {/* </BrowserRouter> */}
-    </HashRouter>
+    </BrowserRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>,
 )
