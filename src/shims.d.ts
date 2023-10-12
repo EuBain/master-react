011817aecc1e7c33d697b2f3b3483186320c58df
EuBain <tayrsi@outlook.com>
2023-10-12 18:13:23 +0800

    nest first

---
+++ src/shims.d.ts
@@ -0,0 +1,9 @@
+/** unocss属性化css添加的ts类型 */
+import { AttributifyAttributes,AttributifyNames} from '@unocss/preset-attributify'
+
+// 添加属性化前缀
+type Prefix = 'u-'
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AttributifyAttributes, Partial<Record<AttributifyNames<Prefix>, string>> {}
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import { defineConfig } from 'vite'
 import unocss from 'unocss/vite'
+// import presetAttributify from '@unocss/preset-attributify'
+// import presetIcons from '@unocss/preset-icons'
+// import presetUno from '@unocss/preset-uno'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import {  presetAttributify, presetUno, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'https://www.tayrsi.cn/master-react/',
-  plugins: [unocss(),react()],
+  plugins: [
+    unocss({
+      presets: [
+        presetUno(),
+        presetAttributify({
+            // 属性化前缀
+            prefix: 'u-',
+            // 强制使用前缀
+            prefixedOnly: false,
+            // 忽略的属性
+            ignoreAttributes: [
+              'text'
+            ]
+          }),
+        presetIcons(),
+       ]
+    }),
+    react()
+  ],
   server: {
     port: 8888, //开发服务器端口
     open: true, //自动打开页面
@@ -17,3 +39,8 @@ export default defineConfig({
     }
   }
 })
+
+function unopreset() {
+  throw new Error('Function not implemented.')
+}
+
