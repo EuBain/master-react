8bc57d2bc4d22a0849c06a5c9283efd253d2e01b
EuBain <tayrsi@outlook.com>
2023-11-05 03:26:31 +0800

    新建分支

--- src/routers/routesMap.tsx
+++ src/routers/routesMap.tsx
@@ -1,19 +1,20 @@
-import { LazyExoticComponent, lazy } from 'react'
+import { LazyExoticComponent, lazy } from "react";
 
+const Home = lazy(() => import("@/pages/Home"));
+const MyLayout = lazy(() => import("@/pages/Layout"));
+const Demo = lazy(() => import("@/pages/demo"));
+const BadServer = lazy(() => import("@/pages/ErrorPage/BedServer"));
+const NotFind = lazy(() => import("@/pages/ErrorPage/NotFind"));
+const Index = lazy(() => import("@/pages/Event"));
 
-
-const Home = lazy(() => import('@/pages/Home'))
-const MyLayout = lazy(() => import('@/pages/Layout'))
-const Demo = lazy(() => import('@/pages/demo'))
-const BadServer = lazy(() => import('@/pages/ErrorPage/BedServer'))
-const NotFind = lazy(() => import('@/pages/ErrorPage/NotFind'))
-const Index = lazy(() => import('@/pages/Event'))
-
-export const component:Record<string, LazyExoticComponent<() => JSX.Element>> = {
-        Home,
-        MyLayout,
-        Demo,
-        BadServer,
-        NotFind,
-        Index,
-}
+export const component: Record<
+  string,
+  LazyExoticComponent<() => JSX.Element>
+> = {
+  Home,
+  MyLayout,
+  Demo,
+  BadServer,
+  NotFind,
+  Index,
+};
